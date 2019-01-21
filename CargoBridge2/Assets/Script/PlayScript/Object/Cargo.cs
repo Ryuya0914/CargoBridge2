73caa79e4957ddc2d10ff62828da1b331de5b185
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-01-21 19:09:48 +0900

    もの運べた

--- CargoBridge2/Assets/Script/PlayScript/Object/Cargo.cs
+++ CargoBridge2/Assets/Script/PlayScript/Object/Cargo.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Cargo : MonoBehaviour {
 
+    public GameObject player;
+    Vector2 playerPos;
+    public Rigidbody2D rb;
+        
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        GetComponent<CircleCollider2D>().enabled = false;
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        playerPos = player.transform.position;
+        if (playerPos.x > (transform.position.x+0.9))
+        {
+            GetComponent<CircleCollider2D>().enabled = true;
+            rb.isKinematic = false;
+        }
 	}
 }
--- CargoBridge2/Assets/Script/PlayScript/Object/Player.cs
+++ CargoBridge2/Assets/Script/PlayScript/Object/Player.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     float speed = 0;
+    //動いているかどうか
     bool move;
+    //目的地
     Vector2 vec;
-    public GameObject fruit;
-    public GameObject house;
+    public GameObject fruit;      
+    public GameObject house;       
+    Vector2 fruitPos;              
+    Vector2 housePos;
     float count = 1;
 
     public LayerMask GroundLayer;
     // Use this for initialization
     void Start () {
+       
+        fruitPos = fruit.transform.position;
+        housePos = house.transform.position;
         move = false;
 	}
 	
@@ -26,31 +33,29 @@ public class Player : MonoBehaviour {
 
         count = -0.01f;
         if (count < 0) count = 0;
-
+        //キャラを動かす処理
         if (move == false)
         {
             if (count == 0)
             {
-                if (Input.GetMouseButtonDown(1))
+                if (Input.GetMouseButtonDown(1))  //右
                 {
                     move = true;
                     speed = 2;
-                    vec = fruit.transform.position;
+                    vec = fruitPos;
                     count = 1;
                 }
-
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButtonDown(0))  //左
                 {
                     move = true;
                     speed = 2;
-                    vec = house.transform.position;
+                    vec = housePos;
                     count = 1;
 
                 }
             }
-
         }
-
+        //キャラを止める処理
         if (move == true)
         {
             if (count == 0)
@@ -67,8 +72,17 @@ public class Player : MonoBehaviour {
         Debug.Log(count);
         Debug.Log(move);
         Debug.Log(speed);
-
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(vec.x, vec.y),
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(vec.x+1, vec.y),
             speed * Time.deltaTime);
+
+        if(transform.position.x>=(fruitPos.x+0.9f))
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+            speed = 0;
+            move = false;
+        }
     }
+
+
+
 }
