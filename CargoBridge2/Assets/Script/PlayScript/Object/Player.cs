35de9b2f1d30b8cf0c7f9337f89b587a85d86975
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-01-21 15:06:12 +0900

    プレイヤーを歩くことができた

--- CargoBridge2/Assets/Script/PlayScript/Object/Player.cs
+++ CargoBridge2/Assets/Script/PlayScript/Object/Player.cs
@@ -4,13 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    float speed = 0;
+    bool move;
+    Vector2 vec;
+    public GameObject fruit;
+    public GameObject house;
+    float count = 1;
+
+    public LayerMask GroundLayer;
+    // Use this for initialization
+    void Start () {
+        move = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        MoveController();
 	}
+    public void MoveController()
+    {
+
+        count = -0.01f;
+        if (count < 0) count = 0;
+
+        if (move == false)
+        {
+            if (count == 0)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    move = true;
+                    speed = 2;
+                    vec = fruit.transform.position;
+                    count = 1;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    move = true;
+                    speed = 2;
+                    vec = house.transform.position;
+                    count = 1;
+
+                }
+            }
+
+        }
+
+        if (move == true)
+        {
+            if (count == 0)
+            {
+                if (Input.GetMouseButtonDown(0)||Input.GetMouseButtonDown(1))
+                {
+                    move = false;
+                    speed = 0;
+                    count = 1;
+                }
+            }
+
+        }
+        Debug.Log(count);
+        Debug.Log(move);
+        Debug.Log(speed);
+
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(vec.x, vec.y),
+            speed * Time.deltaTime);
+    }
 }
