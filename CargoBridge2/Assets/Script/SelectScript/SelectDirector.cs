9e39b49e5e17946072aeae6019e9a1bf55e522f7
sho <nkcgta.sho.takeuchi@gmail.com>
2019-01-21 11:23:30 +0900

    多分ステージセレクト

---
+++ CargoBridge2/Assets/Script/SelectScript/SelectDirector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void back()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void select()
+    {
+        SceneManager.LoadScene("CreateScene");
+    }
+}
