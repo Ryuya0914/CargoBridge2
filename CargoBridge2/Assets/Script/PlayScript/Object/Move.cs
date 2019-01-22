d33b0ebae0ca4816674718a513d2a31f6749fdbc
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-01-22 18:42:57 +0900

    キャラーの動き変更

--- CargoBridge2/Assets/Script/PlayScript/Object/Move.cs
+++ CargoBridge2/Assets/Script/PlayScript/Object/Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-   
+    public int move = 0;
 
     // Use this for initialization
     void Start () {
@@ -14,8 +14,10 @@ public class Move : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
-        Vector3 force = new Vector3(2.0f, 0.0f, 0.0f);    // 力を設定
-        rb.AddForce(force);  // 力を加える
-
+        Vector2 force = new Vector2(move*10.0f, 0.0f);    // 力を設定
+            rb.AddForce(force, ForceMode2D.Force);  // 力を加える
+        Debug.Log(rb.velocity);
+        rb.velocity = new Vector2(Mathf.Max(-3, (Mathf.Min(3, rb.velocity.x))), rb.velocity.y);
+        
     }
 }
--- CargoBridge2/Assets/Script/PlayScript/PlayController.cs
+++ CargoBridge2/Assets/Script/PlayScript/PlayController.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayController : MonoBehaviour {
+public class PlayController : MonoBehaviour
+{
+    //Rayとの当たり判定をレイヤーで設定
+    public LayerMask layerMask;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        Debug.Log("a");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Rayを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10, layerMask);
+            Debug.DrawRay(ray.origin, ray.direction,Color.red);
+            //Rayとなにか衝突したときの処理
+            
+            if (hit.collider)
+            {
+               
+                hit.collider.gameObject.GetComponent<Move>().move = 
+                    (hit.collider.gameObject.GetComponent<Move>(). move==0) ? -1 : 0;
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            //Rayを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10, layerMask);
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+            //Rayとなにか衝突したときの処理
+            
+            if (hit.collider)
+            {
+              
+                hit.collider.gameObject.GetComponent<Move>().move =
+                    (hit.collider.gameObject.GetComponent<Move>().move == 0) ? 1 : 0;
+            }
+        }
+    }
+
 }
