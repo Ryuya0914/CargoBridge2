7fc041c3e189819034ecb59697fbb7f2f1384096
Ryuya0914 <tanaka.ryuya.0914@gmail.com>
2019-01-24 11:16:13 +0900

    ボタンを追加、消しゴム追加、pointが荒ぶるバグ修正、カメラ移動追加

---
+++ CargoBridge2/Assets/Script/CreateScript/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    float cameraSize = 5;
+    float MaxCameraSize;
+    float Top, Bottom, Right, Left;
+
+    void Start() {
+        Top = GameObject.Find("TopRight").transform.position.y - 1;
+        Bottom = GameObject.Find("BottomLeft").transform.position.y + 1;
+        Right = GameObject.Find("TopRight").transform.position.x - 1;
+        Left = GameObject.Find("BottomLeft").transform.position.x + 1;
+        MaxCameraSize = Mathf.Min((Top - Bottom) / 2, (Right - Left) * 8 / 10);
+    }
+
+    //カメラの移動
+    public void Move(Vector3 moveVec) {
+        moveVec.z = 0;
+        Vector3 newPos = transform.position + moveVec / 15f;
+        transform.position = newPos;
+        PosSet();
+    }
+
+    //カメラの位置制限
+    void PosSet() {
+        float cameraPosR = transform.position.x + cameraSize * 8 / 5;
+        float cameraPosL = transform.position.x - cameraSize * 8 / 5;
+        float cameraPosT = transform.position.y + cameraSize;
+        float cameraPosB = transform.position.y - cameraSize;
+        Vector3 newPos = transform.position;
+
+        if (cameraPosR > Right) newPos.x -= cameraPosR - Right;
+        else if (cameraPosL < Left) newPos.x += Left - cameraPosL;
+        if (cameraPosT > Top) newPos.y -= cameraPosT - Top;
+        else if (cameraPosB < Bottom) newPos.y += Bottom - cameraPosB;
+
+        transform.position = newPos;
+    }
+
+    //カメラのズームイン、ズームアウト
+    public void Zoom(float scrollValue) {
+        cameraSize -= scrollValue;
+        cameraSize = Mathf.Clamp(cameraSize, 2.5f, MaxCameraSize);
+        GetComponent<Camera>().orthographicSize = cameraSize;
+        PosSet();
+    } 
+}
--- CargoBridge2/Assets/Script/CreateScript/CreateController.cs
+++ CargoBridge2/Assets/Script/CreateScript/CreateController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CreateController : MonoBehaviour {
     //Rayとの当たり判定をレイヤーで設定
     public LayerMask layerMask;
+    public LayerMask layerMask2;
     public GameObject pointPrefab;
-
-
+    Vector3 backMouseScreenPos = new Vector3(0, 0, 0);
 
     void Update() {
+        //マウスの位置取得
+        Vector3 nowMouseScreenPos = Input.mousePosition;
+        //マウスのホイール入力値取得
+        float wheelScroll = Input.GetAxis("Mouse ScrollWheel");
+
         if (Input.GetMouseButtonDown(0)) {
             //Rayを飛ばす
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -24,6 +29,25 @@ public class CreateController : MonoBehaviour {
                 GetComponent<CreateDirector>().Click(null);
             }
         }
+        if (Input.GetMouseButtonDown(1)) {
+            //Rayを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10, layerMask2);
+            //Rayとなにか衝突したときの処理
+            if (hit.collider) {
+                GameObject obj = hit.collider.gameObject;
+                if (obj.tag == "Walk" || obj.tag == "Wood") {
+                    GetComponent<CreateDirector>().Eraser(obj);
+                }
+            }
+        }
+        if (Input.GetMouseButton(2)) {
+            Camera.main.GetComponent<CameraController>().Move(backMouseScreenPos - nowMouseScreenPos);
+        }
+        if (wheelScroll != 0) {
+            Camera.main.GetComponent<CameraController>().Zoom(wheelScroll);
+        }
+        backMouseScreenPos = nowMouseScreenPos;
     }
 
 }
