0bcb42fb41c7009aa56f626d43d6117c59fa83c0
Ryuya0914 <tanaka.ryuya.0914@gmail.com>
2019-01-24 13:39:03 +0900

    CreateからPlayへ作った橋を持っていく機能追加

--- CargoBridge2/Assets/Script/PlayScript/PlayDirector.cs
+++ CargoBridge2/Assets/Script/PlayScript/PlayDirector.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayDirector : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        Invoke("BridgeStart", 2.0f);
+        GetComponent<PrefabController>().loadPrefab("00");
+    }
+
+    void BridgeStart() {
+        GameObject[] bridges = GameObject.FindGameObjectsWithTag("Walk");
+        for (int i = 0; i < bridges.Length; i++) {
+            bridges[i].GetComponent<Walk>().ObjectModeChange();
+        }
+        bridges = GameObject.FindGameObjectsWithTag("Wood");
+        for (int i = 0; i < bridges.Length; i++) {
+            bridges[i].GetComponent<Wood>().ObjectModeChange();
+        }
+    }
 }
