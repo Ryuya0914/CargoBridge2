e1e842108564ff590744d0d494f5ffa6414cc70e
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-01-30 12:05:11 +0900

    ワープ作る途中

---
+++ CargoBridge2/Assets/Script/PlayScript/Object/WarpChara.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpChara : MonoBehaviour {
+    public enum WarpCharaState
+    {
+        normal,
+        goTooWarpPoint,
+    };
+
+    private WarpCharaState state;
+    private Transform warpPoint;
+    public float walkSpeed;
+    public float goToWaitPointSpeed;
+
+	// Use this for initialization
+	void Start () {
+        state = WarpCharaState.normal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
