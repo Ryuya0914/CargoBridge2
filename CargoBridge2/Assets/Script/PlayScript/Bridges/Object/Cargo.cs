c7214b322731281f75ca48a334ddec156a9a42d5
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-01-30 18:56:10 +0900

    プレイヤー果物の仕様変えた

--- CargoBridge2/Assets/Script/PlayScript/Bridges/Object/Cargo.cs
+++ CargoBridge2/Assets/Script/PlayScript/Bridges/Object/Cargo.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Cargo : MonoBehaviour {
     bool hit = true;
+    bool playFalg = false;
+    void Start()
+    {
+        if (GameDirector.GameState == 1)
+        {
+            playFalg = true;
+            Invoke("ObjectModeChange", 1.8f);
+        }
+    }
+    public void ObjectModeChange()
+    {
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (hit&&col.gameObject.tag == "player" )
+        if (playFalg)
         {
-          
-
-            //if(col.gameObject.GetComponent<DistanceJoint2D>().enabled==false)
-            //{
-            //    col.gameObject.GetComponent<DistanceJoint2D>().enabled = true;
-            //    col.gameObject.GetComponent<DistanceJoint2D>().connectedBody = transform.parent.GetComponent<Rigidbody2D>();
-            //}
-            if (col.gameObject.GetComponent<HingeJoint2D>().enabled == false)
+            if (hit && col.gameObject.tag == "player")
             {
-                col.gameObject.GetComponent<HingeJoint2D>().enabled = true;
-                col.gameObject.GetComponent<HingeJoint2D>().connectedBody = transform.parent.GetComponent<Rigidbody2D>();
-            }
 
+
+                //if(col.gameObject.GetComponent<DistanceJoint2D>().enabled==false)
+                //{
+                //    col.gameObject.GetComponent<DistanceJoint2D>().enabled = true;
+                //    col.gameObject.GetComponent<DistanceJoint2D>().connectedBody = transform.parent.GetComponent<Rigidbody2D>();
+                //}
+                if (col.gameObject.GetComponent<HingeJoint2D>().enabled == false)
+                {
+                    col.gameObject.GetComponent<HingeJoint2D>().enabled = true;
+                    col.gameObject.GetComponent<HingeJoint2D>().connectedBody = transform.parent.GetComponent<Rigidbody2D>();
+                }
+
+            }
         }
+       
     }
 }
--- CargoBridge2/Assets/Script/PlayScript/Bridges/Object/Move.cs
+++ CargoBridge2/Assets/Script/PlayScript/Bridges/Object/Move.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public  bool stop = true;
+    bool playFlag = false;
     public int move = 0;
     private Vector2 m_moveDirection = Vector2.right;
 
@@ -12,25 +13,30 @@ public class Move : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        if (GameDirector.GameState == 1)
+            playFlag = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (stop)
+        if (playFlag)
         {
-            if (move != 0)
+            if (stop)
             {
-                transform.rotation = Quaternion.Euler(0.0f, Mathf.Min(0, 180.0f * move), 0.0f);
+                if (move != 0)
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, Mathf.Min(0, 180.0f * move), 0.0f);
+                }
+                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
+                Vector2 force = new Vector2(move * 10.0f, 0.0f);    // 力を設定
+                rb.AddForce(force, ForceMode2D.Force);  // 力を加える
+
+                rb.velocity = new Vector2(Mathf.Max(-3, (Mathf.Min(3, rb.velocity.x))), rb.velocity.y);
+
             }
-            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
-            Vector2 force = new Vector2(move * 10.0f, 0.0f);    // 力を設定
-            rb.AddForce(force, ForceMode2D.Force);  // 力を加える
-
-            rb.velocity = new Vector2(Mathf.Max(-3, (Mathf.Min(3, rb.velocity.x))), rb.velocity.y);
-
         }
+        
 
     }
 
