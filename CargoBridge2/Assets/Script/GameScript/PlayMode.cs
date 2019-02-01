a6c5100d8df37d2749f9e38b74c21251cd94a8da
Ryuya0914 <tanaka.ryuya.0914@gmail.com>
2019-02-01 10:18:03 +0900

    見た目の大幅な変更、音の追加（途中）

--- CargoBridge2/Assets/Script/GameScript/PlayMode.cs
+++ CargoBridge2/Assets/Script/GameScript/PlayMode.cs
@@ -3,5 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayMode : MonoBehaviour {
+    //Play上でのtexture
+    [SerializeField] Sprite Texture = null;
+    [SerializeField] Color _color = new Color(1f, 1f, 1f, 1f);
 
+    void Start() {
+        if (GameDirector.GameState == 1) {
+            GetComponent<SpriteRenderer>().sprite = Texture;
+            GetComponent<SpriteRenderer>().color = _color;
+        }
+    }
+    
 }
