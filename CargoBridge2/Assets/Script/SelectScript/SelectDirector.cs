18228f7c68378279720bde363a79a931b28c532f
sho <nkcgta.sho.takeuchi@gmail.com>
2019-02-03 09:09:33 +0900

    ステージ

--- CargoBridge2/Assets/Script/SelectScript/SelectDirector.cs
+++ CargoBridge2/Assets/Script/SelectScript/SelectDirector.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SelectDirector : MonoBehaviour {
     string[] stage = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10" };
-    int[] cost = { 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000 };
+    int[] cost = { 2000, 2000, 3000, 0, 2000, 0, 5000, 7000, 2000, 2000 };
 
 	// Use this for initialization
 	void Start () {
